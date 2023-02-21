53b95a6c5e9de66681696ac1f190f90803d0b86a
Andreas Mautsch <amautsch>
2023-02-21 15:38:52 +0100

    hints added

--- src/main/java/org/goafabric/objectstorageservice/Application.java
+++ src/main/java/org/goafabric/objectstorageservice/Application.java
@@ -36,7 +36,28 @@ public class Application {
         public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
             //resources
             hints.resources().registerResource(new ClassPathResource("/com/amazonaws/internal/config/awssdk_config_default.json"));
-            //hints.reflection().registerType(com.amazonaws.internal.config.InternalConfigJsonHelper.class, MemberCategory.INVOKE_DECLARED_METHODS, MemberCategory.INTROSPECT_DECLARED_CONSTRUCTORS, MemberCategory.DECLARED_FIELDS);
+            hints.resources().registerResource(new ClassPathResource("com/amazonaws/partitions/endpoints.json"));
+
+            /*
+            hints.reflection().registerType(com.amazonaws.internal.config.InternalConfigJsonHelper.class, MemberCategory.INVOKE_DECLARED_METHODS, MemberCategory.INVOKE_DECLARED_CONSTRUCTORS);
+            hints.reflection().registerType(com.amazonaws.internal.config.SignerConfigJsonHelper.class, MemberCategory.INVOKE_DECLARED_METHODS, MemberCategory.INVOKE_DECLARED_CONSTRUCTORS);
+            hints.reflection().registerType(com.amazonaws.internal.config.JsonIndex.class, MemberCategory.INVOKE_DECLARED_METHODS, MemberCategory.INVOKE_DECLARED_CONSTRUCTORS);
+            hints.reflection().registerType(com.amazonaws.internal.config.HttpClientConfig.class, MemberCategory.INVOKE_DECLARED_METHODS, MemberCategory.INVOKE_DECLARED_CONSTRUCTORS);
+            hints.reflection().registerType(com.amazonaws.internal.config.HttpClientConfigJsonHelper.class, MemberCategory.INVOKE_DECLARED_METHODS, MemberCategory.INVOKE_DECLARED_CONSTRUCTORS);
+            hints.reflection().registerType(com.amazonaws.internal.config.HttpClientConfig.class, MemberCategory.INVOKE_DECLARED_METHODS, MemberCategory.INVOKE_DECLARED_CONSTRUCTORS);
+            hints.reflection().registerType(com.amazonaws.internal.config.HttpClientConfigJsonHelper.class, MemberCategory.INVOKE_DECLARED_METHODS, MemberCategory.INVOKE_DECLARED_CONSTRUCTORS);
+            hints.reflection().registerType(com.amazonaws.internal.config.HostRegexToRegionMapping.class, MemberCategory.INVOKE_DECLARED_METHODS, MemberCategory.INVOKE_DECLARED_CONSTRUCTORS);
+            hints.reflection().registerType(com.amazonaws.internal.config.HostRegexToRegionMappingJsonHelper.class, MemberCategory.INVOKE_DECLARED_METHODS, MemberCategory.INVOKE_DECLARED_CONSTRUCTORS);
+            hints.reflection().registerType(com.amazonaws.internal.config.EndpointDiscoveryConfig.class, MemberCategory.INVOKE_DECLARED_METHODS, MemberCategory.INVOKE_DECLARED_CONSTRUCTORS);
+
+            hints.reflection().registerType(com.amazonaws.services.s3.internal.AWSS3V4Signer.class, MemberCategory.INVOKE_DECLARED_METHODS, MemberCategory.INVOKE_DECLARED_CONSTRUCTORS);
+
+            try {
+                hints.proxies().registerJdkProxy(org.apache.http.conn.HttpClientConnectionManager.class, org.apache.http.pool.ConnPoolControl.class, Class.forName("com.amazonaws.http.conn.Wrapped"));
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+             */
         }
     }
 
